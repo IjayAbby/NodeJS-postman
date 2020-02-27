30df1678d8f1c7f7d5335d3878566d42afaf1e0f
IjayAbby <inyangasi4@gmail.com>
2020-02-27 18:11:47 +0300

    Backend Development

---
+++ router/task.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const Task = require('../models/task');
+const auth = require('../middleware/auth')
+const router = new express.Router()
+
+
+router.post('/tasks',auth,async(req,res)=>{
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
+
+    try {
+         await task.save()
+        res.status(201).send(task)
+    } catch (e){
+        res.status(400).send(e)
+    }
+})
+
+
+router.get ('/tasks',auth,async(req,res)=>{
+    try {
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks) 
+    } catch(e) {
+        res.status(500).send(e)
+    }
+});
+
+
+router.get('/tasks/:id',async(req,res)=> {
+    const _id = req.params.id
+    try{
+        const task = await Task.findOne({_id,owner:req.user._id})
+            if (!task){
+                return res.status(404).send()
+            }
+        res.send(task)
+    } catch (e){
+        res.status(500).send()
+    }
+})
+
+router.patch('/tasks/:id',auth,async(req,res) => {
+    const updates = Object.keys(req.body);
+    const allowedupdates = ['description','completed'];
+    const isValidOperation = updates.every((update)=> allowedupdates.includes(update));
+    if (!isValidOperation){
+        return res.status(400).send({error : 'Invalid updates'})
+    };
+    
+    try { 
+        const task = await Task.findOne({_id:req.params.id, owner : req.user._id})
+        if (!task){
+            return res.status(404).send()
+        } 
+        updates.forEach((update)=> task[update] = req.body[update]);
+        await task.save();
+        res.send(task)
+     } catch (e){
+        res.status(400).send(e)
+    }    
+});
+
+router.delete('/tasks/:id',async(req,res)=>{
+    try {
+        const task = await Task.findByIdAndDelete({_id : req.params.id,owner:req.user._id})
+        if (!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e)
+{
+    res.status(500).send();
+}})
+
+module.exports = router;
---
+++ router/user.js
@@ -0,0 +1,105 @@
+const express = require ('express')
+const User = require('../models/user');
+const auth = require('../middleware/auth')
+const router = new express.Router()
+
+router.post('/users',async(req,res)=>{
+    const user = new User(req.body);
+
+    try {
+        await user.save();
+        const token = await user.generateAuthToken()
+        res.status(201).send({user,token});
+    } catch (e){
+        res.status(400).send(e)
+    }
+})
+
+router.post('/users/login',async(req,res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email,req.body.password);
+        const token = await user.generateAuthToken();
+        res.send({user,token});
+    } catch (e){
+        res.status(400).send(e)
+    }
+})
+
+router.post('/users/logout',auth,async(req,res)=>{
+    try{
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        })
+        await req.user.save();
+        res.send()
+    } catch (e){
+        res.status(500).send(e)
+    }
+})
+
+router.post('/users/logoutAll',auth,async(req,res)=>{
+    try{
+        req.user.tokens = [];
+        await req.user.save();
+        res.send()
+    
+    } catch (e){
+        res.status(500).send(e)
+    }
+})
+
+
+router.get ('/users/me', auth, async(req,res)=>{
+    res.send(req.user)
+})
+
+// router.get('/users/:id',async(req,res)=>{
+//     const _id = req.params.id;
+
+//     try{
+//         const user = await User.findById(_id);
+//         if (!user){
+//             return res.status(404).send()
+//         }
+//         res.send(user)
+//     } catch (e){
+//         res.status(500).send()
+//     }
+// })
+
+router.patch('/users/me',auth,async(req,res) => {
+    const updates = Object.keys(req.body);
+    const allowedupdates = ['name','email','password','age'];
+    const isValidOperator = updates.every((update)=> allowedupdates.includes(update));
+    if (!isValidOperator){
+        return res.status(400).send({error : 'Invalid updates'})
+    };
+    
+    try { 
+        
+        updates.forEach((update) => req.user[update] = req.body[update]);
+        await req.user.save();
+        
+        
+            
+         res.send(req.user)
+    
+    } catch (e){
+        res.status(400).send(e)
+    }    
+});
+  
+
+router.delete('/users/me',auth,async(req,res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e){
+        res.status(500).send();
+    }
+})
+
+module.exports = router;
